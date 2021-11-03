edc10c0a025ff31421cea977165c1f698081e54e
Tori1Mana5 <toriimana5@yahoo.co.jp>
2021-11-03 22:01:17 +0900

    入力画面、一覧表示画面作成

---
+++ form/input.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+  <form method="post" action="../index/index.php">
+    やること
+    <input type="text">
+    <br>
+    <input type="submit" value="確認する">
+  </form>
+
+</body>
+</html>
---
+++ index/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+  TO DO
+  <?php echo '<ul>'?>
+    <?php echo '<li>'?>
+    <?php echo 'PHPの勉強'?>
+    <?php echo '</li>'?>
+    <?php echo '<li>'?>
+    <?php echo '仕事'?>
+    <?php echo '</li>'?>
+  <?php echo '</ul>'?>
+</body>
+</html>
