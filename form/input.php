234a67ce4b16a05f4c462bf3d510af4d8eaf3c39
Tori1Mana5 <toriimana5@yahoo.co.jp>
2021-11-03 22:10:33 +0900

    サニタイズ設定等

--- form/input.php
+++ form/input.php
@@ -1,9 +1,16 @@
+<?php 
+function h($str){
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+?>
+
 <!DOCTYPE html>
 <head></head>
 <body>
+
   <form method="post" action="../index/index.php">
     やること
-    <input type="text">
+    <input type="text" name="todo" value="<?php if(!empty($_POST['todo'])){echo h($_POST['todo']) ;}?>" required>
     <br>
     <input type="submit" value="確認する">
   </form>
