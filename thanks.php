4fef8cb494fdd0e38d23e1211d94a287aa17caf9
Tori1Mana5 <toriimana5@yahoo.co.jp>
2021-12-06 23:32:27 +0900

    完了画面を作成

---
+++ thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>登録完了</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+  やることを作成しました！
+  <br>
+  <a href="index.php">一覧に戻る</a>
+</body>
+</html>
