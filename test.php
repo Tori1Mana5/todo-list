058405edce4428e3c5c88329d3527e1c3c1d53bb
Tori1Mana5 <toriimana5@yahoo.co.jp>
2022-01-09 23:25:29 +0900

    php勉強

--- test.php
+++ test.php
@@ -1,2 +1,7 @@
-<?= 'PHP こんにちは！'?>
-<?php echo 'こんにちは！まなこさん'?>
+<?php $expression = 'あああ'; ?>
+
+<?php if($expression == true): ?>
+  trueならばここが表示
+<?php else: ?>
+  true以外ならばここが表示されます
+<?php endif; ?>
