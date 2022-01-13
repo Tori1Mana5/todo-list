5650c72392ca2dc7972ccbc620f299001322f825
Tori1Mana5 <toriimana5@yahoo.co.jp>
2022-01-13 15:01:27 +0900

    名前空間実装

--- call.php
+++ call.php
@@ -2,4 +2,4 @@
 require_once 'test_1.php';
 require_once 'test_2.php';
 
-echo getGreeting();
+echo test\getGreeting();
--- test_1.php
+++ test_1.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace test;
 function getGreeting(){
   return 'おっはー';
 }
