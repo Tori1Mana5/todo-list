47332794eee54e739b975c0876d36d85264155c6
Tori1Mana5 <toriimana5@yahoo.co.jp>
2022-01-15 08:43:36 +0900

    インスタンスの学習をしました

--- test_1.php
+++ test_1.php
@@ -1,5 +1,28 @@
 <?php
-namespace test;
-function getGreeting(){
-  return 'おっはー';
+
+class Person
+{
+  private $name;
+  public $age;
+
+  function __construct($name, $age)
+  {
+    $this->name = $name;
+    $this->age = $age;
+  }
+
+  function hello(){
+    echo 'hello, ' . $this->name;
+    return $this;
+  }
+
+  function bye(){
+    echo 'bye, ' . $this->name;
+    return $this;
+  }
 }
+
+$bob = new Person('Bob', 18);
+$bob->hello()->bye();
+// $tim = new Person('Tim', 18);
+// $tim->hello();
